7776fc0b854585e5d7209b990e87fe79f8dd2da9
NatM <altergifted@gmail.com>
2022-05-29 20:50:31 +0700

    initial commit

---
+++ projet_1/1.py
@@ -0,0 +1,12 @@
+from re import A
+import numpy as np
+print (np.__version__)
+a=np.int8(25)
+print(type(a))
+print(a)
+b=a
+print(type(b))
+print(np.iinfo (np.int64))
+print(np.finfo(np.float16))
+a=np.uint8(-456)
+print(a)
---
+++ projet_1/arr.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+
+simplelist = [19, 242, 14, 152, 142, 1000]
+simplelist=np.array(simplelist)
+print(simplelist.mean())
